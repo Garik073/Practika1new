d0bb3a3ffc343edc43c0c7780d3231125b09c64e
Igor <gariklukin073@gmail.com>
2022-11-29 01:31:09 +0400

    New vesion

--- Home work 3/3.0/Program.cs
+++ Home work 3/3.0/Program.cs
@@ -4,21 +4,11 @@
    double pere;
    while (i < N )
    {
-        i += 1;
-        if (i == (N))
-        {
-            pere = Math.Pow(i,3); // за основу взяли задачу с семинара и просто вместо 2 пишем 3
-            Console.Write($"{pere}");
-        }
-        else
-        {
-        
-        pere = Math.Pow(i,3);
+        i ++;
+        pere = Math.Pow(i,3); 
         Console.Write($"{pere},");
-        }
-   }
     
-   
+}
 }
 Console.WriteLine("Введите число");
 int chislo = int.Parse(Console.ReadLine());

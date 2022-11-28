5e98fbaaf79238a8c7b947c902298ab7399ada5e
Igor <gariklukin073@gmail.com>
2022-11-28 23:47:06 +0400

    commit one work

--- Home work 3/1.0/Program.cs
+++ Home work 3/1.0/Program.cs
@@ -1,10 +1,11 @@
 ﻿Console.WriteLine("Введите число: ");
-string number = Console.ReadLine();
-int a = number.Length;
+string number = Console.ReadLine(); // Масиив
+int a = number.Length; // Длина массива
 
 if (a == 5)
 {
-    if (number[0] == number[4] && number[1] == number[3])
+    if (number[0] == number[4] && number[1] == number[3]) // Проверяет массив на совпадения. Массив всегда ничинаеться с 0.
+                                                          //Поэтому проверяем чтобы ячеки 0 и 4 соподали позначению так же и с ячеками 1 и 3
     {
         Console.WriteLine($"{number} - Палиндром");
     }
@@ -15,5 +16,5 @@ if (a == 5)
 }
 else
 {
-    Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
+    Console.WriteLine($"ОШИБКА: {number} - не является пятизначным"); // в вели больше 5 чисел что не соответсвует условиям
 }

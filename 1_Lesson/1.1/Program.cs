3d097112293145cc17abcea0ca146956c439b603
Igor <gariklukin073@gmail.com>
2022-11-28 18:44:08 +0400

    Practika1.1

---
+++ 1_Lesson/1.1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ведите число");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ведите второе число");
+int num2 = int.Parse(Console.ReadLine());
+
+if(num2 == num1*num1)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}

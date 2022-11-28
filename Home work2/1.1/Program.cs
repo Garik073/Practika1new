56cfff8b29fecb3ca556ce6525eeabfb4ef371f5
Igor <gariklukin073@gmail.com>
2022-11-28 19:19:19 +0400

    One work2.1

---
+++ Home work2/1.1/Program.cs
@@ -0,0 +1,25 @@
+﻿int number = ReadInt("Введите трехзначное число: ");
+int number2 = number.ToString().Length;
+
+if (number2 < 3 || number2 > 3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    Console.WriteLine(InCenter(number));
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int InCenter(int a)
+{
+    
+    int result = ((a / 10) % 10);
+    return result;
+

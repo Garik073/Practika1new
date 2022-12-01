f16b6a0905e65a283e961bd64092bd5aa9cdd34f
Igor <gariklukin073@gmail.com>
2022-12-02 00:00:13 +0400

    HomeWork4.1

---
+++ Home work4/1.0/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+void Chet( int A,int B)
+{
+    int summa = A;
+  
+  for (int i = 1;i < B; i++)
+  {
+    summa = summa * A;
+  }
+  Console.Write($" Сумма в степени числа В {summa}");
+    
+}
+
+Console.WriteLine("Введите число A");
+int ChisloA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int ChisloB = int.Parse(Console.ReadLine());
+Chet(ChisloA,ChisloB);

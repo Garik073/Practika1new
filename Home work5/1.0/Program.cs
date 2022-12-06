795c55b25466eeee5be7a83bc38141fbf9f2fbb3
Igor <gariklukin073@gmail.com>
2022-12-06 19:42:12 +0400

    HomeWork5.1

---
+++ Home work5/1.0/Program.cs
@@ -0,0 +1,34 @@
+﻿void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] Mass(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(100,999);
+    return arr;
+}
+
+void Poisk(int[] arr)
+{
+    int Chetnoe = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] % 2 == 0)
+             Chetnoe++;
+    }
+    Console.WriteLine($"Количество четных чисел в массиве: {Chetnoe}");
+}
+
+int[] arr_1 = Mass(int.Parse(Console.ReadLine()));
+Print(arr_1);
+Poisk(arr_1);
+
+

a10f166096f19584546969979fd956600c3f0ace
Igor <gariklukin073@gmail.com>
2022-12-22 22:19:52 +0400

    HomeWork9.2

---
+++ Home work9/2.0/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите начальное число M:");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число M:");
+int numberN = int.Parse(Console.ReadLine());
+
+
+void SumNumbers (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    SumNumbers(numberM, numberN, sum);
+}
+
+SumNumbers(numberM, numberN, 0);
